fc37274dcf0fd6a20f15b59deaa260799bd741e7
Rojus Lapinskas <rojuslapinskas@gmail.com>
2023-03-17 20:31:37 +0100

    added blank card and glowy buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/Card";
+import { AiFillHeart } from "react-icons/ai";
+import { RxCross1 } from "react-icons/rx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black w-full h-screen">
+      <div className="text-3xl text-white"></div>
+      <div className="pt-32">
+        <Card />
+      </div>
+      <div className="text-center flex items-center justify-center mt-7 text-3xl relative">
+        <div>
+          <div className="absolute bg-neutral-400 mx-8 p-3 w-12 h-12 rounded-full hover:blur duration-300 cursor-pointer"></div>
+          <RxCross1 className="text-neutral-400 mx-8 p-3 box-border outline outline-neutral-400 rounded-full w-12 h-12 relative bg-black pointer-events-none" />
+        </div>
+        <div>
+          <div className="absolute bg-green-600 mx-8 p-3 w-12 h-12 rounded-full hover:blur duration-300 cursor-pointer"></div>
+          <AiFillHeart className="text-green-600 mx-8 p-3 box-border outline outline-green-600 rounded-full w-12 h-12 relative bg-black pointer-events-none" />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="text-center flex items-center justify-center">
+      <div className="aspect-[5/7] w-[30vmin] bg-white rounded-3xl">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
